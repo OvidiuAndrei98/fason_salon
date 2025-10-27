3be2527673a6eebe10282fab18334726aa41bc33
OvidiuAndrei98 <andrei.penica@gmail.com>
2025-10-27 17:37:58 +0200

    updated hero section

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 
 const HeroSection = () => {
   return (
-    <div className="relative flex flex-col h-full p-[var(--spacing-s)]">
+    <div className="relative flex flex-col h-[calc(100vh-56px)] p-[var(--spacing-s)]">
       <div className="flex flex-col">
         <h1 className=" font-heading text-secondary-foreground pt-[16px] font-bold text-[clamp(2rem,_15vw,_10rem)] uppercase leading-none tracking-wide m-0 p-0 text-nowrap">
           CHRISTINA GRAMMA
@@ -19,12 +19,12 @@ const HeroSection = () => {
         src={HeroImage}
         alt="hero-image"
       />
-      <div className="w-full px-4 flex flex-col mt-auto relative">
+      <div className="w-full h-[45%] px-4 flex flex-col mt-auto relative justify-end gap-[var(--spacing-l)] pb-4">
         <div className="absolute top-[-70px] left-0 w-full h-[80px] bg-gradient-to-t from-background via-background to-transparent" />
         <Button className="h-[80px] self-center text-xl bg-foreground text-background hover:bg-accent active:bg-accent hover:text-secondary-background">
           Explore My Book
         </Button>
-        <span className="text-center text-secondary-foreground order-first">
+        <span className="text-center text-xl text-secondary-foreground order-first">
           Styling-ul, pentru mine, este arta de a modela frumusețea interioară.
           Creez imagini prin culoare și tunsoare. Cartea mea împărtășește
           viziunea din spatele fiecărei transformări.
