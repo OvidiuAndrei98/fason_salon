238dbe96597998d0b91580401a5ad35dca359c53
OvidiuAndrei98 <andrei.penica@gmail.com>
2025-10-27 17:30:27 +0200

    updated hero section

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,29 +1,34 @@
 import Image from "next/image";
 import HeroImage from "@/public/hero_image.webp";
+import { Button } from "@/components/ui/button";
 
 const HeroSection = () => {
   return (
-    <div className="relative flex flex-col ">
+    <div className="relative flex flex-col h-full p-[var(--spacing-s)]">
       <div className="flex flex-col">
-        <h1 className=" font-heading text-secondary-foreground pt-[50px] font-bold text-[clamp(2rem,_15vw,_10rem)] uppercase leading-none tracking-wide m-0 p-0 text-nowrap">
+        <h1 className=" font-heading text-secondary-foreground pt-[16px] font-bold text-[clamp(2rem,_15vw,_10rem)] uppercase leading-none tracking-wide m-0 p-0 text-nowrap">
           CHRISTINA GRAMMA
         </h1>
         <h2 className="self-end font-heading text-[clamp(1rem,_3vw,_1.5rem)] font-extrabold tracking-wider bg-gradient-to-r from-slate-700 via-gray-500 to-slate-700 bg-clip-text text-transparent">
           ARTE împletite în PĂR
         </h2>
-        <Image
-          className="absolute top-[55%] mx-auto left-[50%] translate-x-[-50%] w-[90vw] max-w-[700px] h-auto"
-          src={HeroImage}
-          alt="hero-image"
-        />
       </div>
 
-      <div className="w-full max-w-[600px]  text-center text-secondary-foreground px-4">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries.
+      <Image
+        className="absolute top-[7%] mx-auto left-[50%] translate-x-[-50%] w-[100vw] max-w-[700px] h-auto"
+        src={HeroImage}
+        alt="hero-image"
+      />
+      <div className="w-full px-4 flex flex-col mt-auto relative">
+        <div className="absolute top-[-70px] left-0 w-full h-[80px] bg-gradient-to-t from-background via-background to-transparent" />
+        <Button className="h-[80px] self-center text-xl bg-foreground text-background hover:bg-accent active:bg-accent hover:text-secondary-background">
+          Explore My Book
+        </Button>
+        <span className="text-center text-secondary-foreground order-first">
+          Styling-ul, pentru mine, este arta de a modela frumusețea interioară.
+          Creez imagini prin culoare și tunsoare. Cartea mea împărtășește
+          viziunea din spatele fiecărei transformări.
+        </span>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen items-center m-[0_auto] max-w-[1024px] bg-background font-sans dark:bg-black">
       <nav className="flex justify-center w-full px-8">
-        <div className="flex  justify-between w-full  py-8">
+        <div className="flex  justify-between w-full  py-4">
           <p className="font-sans text-secondary-foreground">LOGO</p>
           <ul className="flex gap-10 text-secondary-foreground">
             <li>[HOME]</li>
