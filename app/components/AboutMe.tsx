85c7526292e9603d3effad7dc2c1ae79a4cc501a
Francesco-Zucco <zuccoqwe@gmail.com>
2025-11-06 23:17:09 +0200

    fixes in about me

--- app/components/AboutMe.tsx
+++ app/components/AboutMe.tsx
@@ -1,14 +1,16 @@
 const AboutMe = () => {
   return (
-    <div className="flex flex-col items-center w-full px-[var(--spacing-s)]">
+    <div className="flex flex-col items-center w-full ">
       <h2 className="font-heading text-3xl md:text-4xl py-[30px] md:py-[50px] font-extrabold tracking-wide bg-gradient-to-r from-slate-700 via-gray-500 to-slate-700 bg-clip-text text-transparent">
         Viziunea Mea
       </h2>
-      <div className="w-full flex flex-col md:flex-row gap-10 md:gap-4 max-w-[1024px]">
-        <div className="bg-[#d6d6d6] flex flex-col gap-4 py-[50px] pl-2 pr-[50px] text-xl before:absolute before:content-[''] before:text-[100px] before:text-gray-400 before:opacity-20 before:absolute before:-top-10 before:-left-2 relative rounded-4xl shadow-lg">
-          <span>Despre mine</span>
-          <h2>CHRISTINA GRAMMA</h2>
-          <p>
+      <div className="w-full flex flex-col space-between md:flex-row gap-10  w-full">
+        <div className="bg-[#d6d6d6] flex flex-col gap-4  [padding-left:clamp(0.5rem,calc((100vw-1024px)/2),50vw)] pr-[50px] py-[50px]  text-xl rounded-tr-4xl rounded-br-4xl">
+          <span className="text-accent text-2xl">Despre mine</span>
+          <h2 className="text-secondary-foreground font-heading text-4xl font-extrabold tracking-wide">
+            CHRISTINA GRAMMA
+          </h2>
+          <p className="pb-2">
             Cu o experiență de peste 10 ani în domeniul hair styling-ului, sunt
             dedicată să creez look-uri care să reflecte personalitatea unică a
             fiecărui client. Specializată în tehnici moderne de colorare și
@@ -23,12 +25,9 @@ const AboutMe = () => {
             în realitate.
           </p>
         </div>
-        <div className="w-full">
-          <div
-            className="flex flex-col justify-end p-[32px] bg-[linear-gradient(180deg,rgba(204,204,204,0)_14%,rgba(0,0,0,1)_100%),url('/CHRISTINA.jpg')] w-[400px] h-[100%] object-contain bg-center bg-no-repeat bg-cover  m-auto md:m-0 mb-10 md:mb-0 rounded-4xl shadow-lg 
-          s"
-          >
-            <div className="flex gap-2 flex-nowrap">
+        <div className="w-full [padding-right:clamp(0.5rem,calc((100vw-1024px)/2),50vw)] px-2">
+          <div className="aspect-3/4 flex flex-col gap-2 justify-end p-[32px] bg-[linear-gradient(180deg,rgba(204,204,204,0)_14%,rgba(0,0,0,1)_100%),url('/CHRISTINA.jpg')] min-w-[350px] h-[100%] object-contain bg-center bg-no-repeat bg-cover  m-auto md:m-0 mb-10 md:mb-0 rounded-4xl shadow-[2px_4px_44px_0px_rgba(0,_0,_0,_0.8)]">
+            <div className="flex gap-1 flex-nowrap">
               <span className="bg-[#8484847e] text-foregroud p-[10px] text-[#ececec]  rounded-full text-xs">
                 HAIR DESIGN
               </span>
