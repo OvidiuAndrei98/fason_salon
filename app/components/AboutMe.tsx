326617c61b00e80a0087faa9e2b5f6d42d5e5315
Francesco-Zucco <zuccoqwe@gmail.com>
2025-11-06 22:50:53 +0200

    added about me

--- app/components/AboutMe.tsx
+++ app/components/AboutMe.tsx
@@ -1,51 +1,49 @@
 const AboutMe = () => {
   return (
-    <div className="max-w-[1024px] flex flex-col items-center w-full px-[var(--spacing-s)]">
+    <div className="flex flex-col items-center w-full px-[var(--spacing-s)]">
       <h2 className="font-heading text-3xl md:text-4xl py-[30px] md:py-[50px] font-extrabold tracking-wide bg-gradient-to-r from-slate-700 via-gray-500 to-slate-700 bg-clip-text text-transparent">
         Viziunea Mea
       </h2>
-      <div className="w-full flex flex-col items-center gap-4">
-        <div className="h-[150px] w-[150px] md:h-[250px] md:w-[250px] bg-[url('/image16.jpeg')] bg-cover rounded-full"></div>
-        <h3 className="text-secondary-foreground font-heading text-2xl font-extrabold tracking-wide">
-          Christina Gramma
-        </h3>
-        <ul className="flex flex-wrap max-w-[430px] justify-center items-center gap-2">
-          <li className="inline-flex items-center rounded-full bg-blue-100 border border-blue-200 p-[2px_4px] text-sm font-medium text-blue-800">
-            HAIR DESIGN
-          </li>
-
-          <li className="inline-flex items-center rounded-full bg-gray-100 border border-gray-200 p-[2px_4px] text-sm font-medium text-gray-700">
-            HAIRSTYLIST
-          </li>
-
-          <li className="inline-flex items-center rounded-full bg-violet-100 border border-violet-200 p-[2px_4px] text-sm font-medium text-violet-800">
-            COLORIST
-          </li>
-
-          <li className="inline-flex items-center rounded-full bg-emerald-100 border border-emerald-200 p-[2px_4px] text-sm font-medium text-emerald-800">
-            BALAYAGE AVANSAT
-          </li>
-        </ul>
-      </div>
-      <div className="grid grid-cols-1 grid-rows-[300px_1fr] md:grid-rows-1 md:grid-cols-2 gap-4 w-full mt-4 md:mt-8 mb-16">
-        <div className="bg-[url('/image23.jpeg')] bg-cover rounded-lg relative before:rounded-lg before:absolute before:inset-0 before:bg-gradient-to-l before:from-black/50 before:to-black/5"></div>
-        <div className="flex flex-col justify-top gap-6">
-          <blockquote className="relative border-t-4 border-r-0 md:border-t-0 md:border-r-4 border-accent pt-6 md:pr-6 md:pt-0 text-center md:text-right">
-            <p className="text-md md:text-lg text-muted-foreground leading-relaxed">
-              Cu o experiență de peste 10 ani în domeniul hair styling-ului,
-              sunt dedicată să creez look-uri care să reflecte personalitatea
-              unică a fiecărui client. Specializată în tehnici moderne de
-              colorare și balayage, îmbin creativitatea cu precizia pentru
-              rezultate excepționale.
-            </p>
-            <p className="text-md md:text-lg text-muted-foreground leading-relaxed mt-4">
-              Filosofia mea se bazează pe îngrijirea și sănătatea părului,
-              folosind produse premium și tehnici inovatoare pentru a obține
-              cele mai bune rezultate. Fiecare vizită în salonul meu este o
-              experiență personalizată, unde ascult cu atenție dorințele tale și
-              le transform în realitate.
-            </p>
-          </blockquote>
+      <div className="w-full flex flex-col md:flex-row gap-10 md:gap-4 max-w-[1024px]">
+        <div className="bg-[#d6d6d6] flex flex-col gap-4 py-[50px] pl-2 pr-[50px] text-xl before:absolute before:content-[''] before:text-[100px] before:text-gray-400 before:opacity-20 before:absolute before:-top-10 before:-left-2 relative rounded-4xl shadow-lg">
+          <span>Despre mine</span>
+          <h2>CHRISTINA GRAMMA</h2>
+          <p>
+            Cu o experiență de peste 10 ani în domeniul hair styling-ului, sunt
+            dedicată să creez look-uri care să reflecte personalitatea unică a
+            fiecărui client. Specializată în tehnici moderne de colorare și
+            balayage, îmbin creativitatea cu precizia pentru rezultate
+            excepționale.
+          </p>
+          <p>
+            Filosofia mea se bazează pe îngrijirea și sănătatea părului,
+            folosind produse premium și tehnici inovatoare pentru a obține cele
+            mai bune rezultate. Fiecare vizită în salonul meu este o experiență
+            personalizată, unde ascult cu atenție dorințele tale și le transform
+            în realitate.
+          </p>
+        </div>
+        <div className="w-full">
+          <div
+            className="flex flex-col justify-end p-[32px] bg-[linear-gradient(180deg,rgba(204,204,204,0)_14%,rgba(0,0,0,1)_100%),url('/CHRISTINA.jpg')] w-[400px] h-[100%] object-contain bg-center bg-no-repeat bg-cover  m-auto md:m-0 mb-10 md:mb-0 rounded-4xl shadow-lg 
+          s"
+          >
+            <div className="flex gap-2 flex-nowrap">
+              <span className="bg-[#8484847e] text-foregroud p-[10px] text-[#ececec]  rounded-full text-xs">
+                HAIR DESIGN
+              </span>
+              <span className="bg-[#8484847e] text-foregroud p-[10px] text-[#ececec]  rounded-full text-xs">
+                HAIRSTYLIST
+              </span>
+              <span className="bg-[#8484847e] text-foregroud p-[10px] text-[#ececec]  rounded-full text-xs">
+                COLORIST
+              </span>
+            </div>
+            <div className="text-white">
+              Styling-ul e arta de a modela frumusețea interioară. Creez imagini
+              prin culoare și tunsoare.
+            </div>
+          </div>
         </div>
       </div>
     </div>
